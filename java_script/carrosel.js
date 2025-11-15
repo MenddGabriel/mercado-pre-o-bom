6b1874ab5e67df6fdaec1ca34a2c9c824da5aa4f
pr223071 <pr223071@gmail.com>
2025-11-15 11:31:39 -0300

    carrosel pronto

---
+++ java_script/carrosel.js
@@ -0,0 +1,36 @@
+// Aguarda o DOM (estrutura HTML) estar completamente carregado
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleciona o container principal
+  const carrossel = document.querySelector("#carrossel");
+
+  if (!carrossel) {
+    // erro
+    console.error(
+      "Erro JS: Elemento do carrossel com ID '#carrossel' não encontrado!"
+    );
+    return;
+  }
+
+  //avancar
+  function showNextSlide() {
+    const slidesList = carrossel.querySelector(".listas");
+
+    // busca slide ativo
+    const activeSlide = slidesList.querySelector(".imagem[data-active]");
+
+    // Pega a lista completa de slides (todas as LIs com classe 'imagem')
+    const slides = [...slidesList.querySelectorAll(".imagem")];
+
+    let newIndex = slides.indexOf(activeSlide) + 1;
+
+    if (newIndex >= slides.length) {
+      newIndex = 0;
+    }
+
+    // Troca o slide ativo
+    delete activeSlide.dataset.active;
+    slides[newIndex].dataset.active = true;
+  }
+
+  setInterval(showNextSlide, 4000);
+});
---
+++ java_script/contato.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+
+  if (!form) {
+    console.error("Erro: O formulário HTML não foi encontrado!");
+    return;
+  }
+
+  // Adiciona o listener APÓS o DOM estar carregado
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio normal do formulário
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const msg = document.getElementById("msg").value;
+
+    const telefone = "5548991149054";
+
+    // Monta a mensagem
+    const texto = `*Contato pelo site*\n
+*Nome:* ${nome}
+*Email:* ${email}
+*Mensagem:* ${msg}`;
+
+    // monta a URL do WhatsApp
+    const url = `https://wa.me/${telefone}?text=${encodeURIComponent(texto)}`;
+
+    // Abre o WhatsApp em uma nova aba
+    window.open(url, "_blank");
+  });
+});
